756a04b27dfecffecc263e43b12f0847b1392101
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-06-30 14:05:11 -0500

    added better syntax

--- bizzbuzz.js
+++ bizzbuzz.js
@@ -2,7 +2,7 @@
 let i=0
 while (i < 100){
     i++
-    if (i % 3 === 0 && i % 5 === 0 ){
+    if ((i % 3 === 0) && (i % 5 === 0 )) {
         console.log(`fizzbuzz`)
     }else if (i % 3 === 0) {
         console.log(`fizz`)
